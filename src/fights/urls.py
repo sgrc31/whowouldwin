03c5447b50b01f971d7dce68291a227e8fa9f520
Simone Guercio <s.guercio@gmail.com>
2022-07-04 15:24:48 +0200

    Add fight detail view and template.

--- src/fights/urls.py
+++ src/fights/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
-from .views import HomePageView
+from .views import HomePageView, FightDetailView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('fights/', RedirectView.as_view(pattern_name='home'),
+         name='fights_list'),
+    path('fights/<slug:suuid>/', FightDetailView.as_view(), name='fight_detail'),
     ]
--- src/fights/views.py
+++ src/fights/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Fight
 
 
@@ -6,3 +6,10 @@ class HomePageView(ListView):
     template_name = 'home.html'
     model = Fight
     context_object_name = 'active_fights_list'
+
+
+class FightDetailView(DetailView):
+    template_name = 'fight_detail.html'
+    model = Fight
+    slug_field = 'small_uuid'
+    slug_url_kwarg = 'suuid'
