306fb9fec4f4bbb6811101b30657c9f584b03dc7
Simone Guercio <s.guercio@gmail.com>
2022-07-04 17:07:52 +0200

    Add basic test for db models

--- tests/test_fights.py
+++ tests/test_fights.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse, resolve
+from fights.models import Fighter
 
 
 class TestHomepage:
@@ -16,3 +17,18 @@ class TestHomepage:
         response = client.get(reverse('home'))
         assert response.status_code == 200
         assert response.template_name[0] == 'home.html'
+
+
+class TestFighterModel:
+    @pytest.mark.django_db
+    def test_fighter_count(self):
+        assert Fighter.objects.count() == 0
+
+    @pytest.mark.django_db
+    def test_fighter_create(self):
+        Fighter.objects.create(name="Tizio",
+                               description="descrizione tizio",
+                               powers="poteri tizio")
+        assert Fighter.objects.count() == 1
+        test_fighter_1 = Fighter.objects.get(id=1)
+        assert test_fighter_1.name == "Tizio"
